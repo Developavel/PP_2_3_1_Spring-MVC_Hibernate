1c0c6d389b235c379225629938bb332d142496bb
Developavel <pa.mordovin@gmail.com>
2026-04-27 20:34:11 +0300

    Update User.java

--- src/main/java/web/model/User.java
+++ src/main/java/web/model/User.java
@@ -7,4 +7,48 @@ public class User {
     private String name;
     private String lastName;
 
+    public User() {
+    }
+
+    public User(int id, String name, String lastName) {
+        this.id = id;
+        this.name = name;
+        this.lastName = lastName;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (o == null || getClass() != o.getClass()) return false;
+        User user = (User) o;
+        return id == user.id && Objects.equals(name, user.name) && Objects.equals(lastName, user.lastName);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id, name, lastName);
+    }
 }
