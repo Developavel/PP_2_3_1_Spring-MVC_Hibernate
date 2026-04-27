93512d2c7ea16f7e8b91c5c256b60333b90f0f48
Developavel <pa.mordovin@gmail.com>
2026-04-27 20:48:04 +0300

    Update UserServiceImpl.java

--- src/main/java/web/service/UserServiceImpl.java
+++ src/main/java/web/service/UserServiceImpl.java
@@ -1,4 +1,15 @@
 package web.service;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import web.dao.UserDao;
+
 public class UserServiceImpl implements UserService {
+    private final UserDao userDao;
+
+    @Autowired
+    public UserServiceImpl(UserDao userDao) {
+        this.userDao = userDao;
+    }
+
+
 }
