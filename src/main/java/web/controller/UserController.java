348183c7afb8836518e45a836a79baf61e84408b
Developavel <pa.mordovin@gmail.com>
2026-04-27 20:40:21 +0300

    Update UserController.java

--- src/main/java/web/controller/UserController.java
+++ src/main/java/web/controller/UserController.java
@@ -2,32 +2,19 @@ package web.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import web.service.CarService;
+import web.service.UserService;
 
 @Controller
-@RequestMapping("/cars")
+@RequestMapping("/users")
 public class UserController {
 
-    private final CarService carService;
+    private final UserService userService;
 
     @Autowired
-    public UserController(CarService carService) {
-        this.carService = carService;
+    public UserController(UserService userService) {
+        this.userService = userService;
     }
 
-    @GetMapping
-    public String showAllCars(Model model) {
-        model.addAttribute("cars", carService.getAllCars());
-        return "cars";
-    }
 
-    @GetMapping(params = "count")
-    public String showCarsWithCount(@RequestParam(value = "count") int count, Model model) {
-        model.addAttribute("cars", carService.getCars(count));
-        return "cars";
-    }
 }
