dd8cabfc7e45ee8b51401508eedf967daa394392
Developavel <pa.mordovin@gmail.com>
2026-04-28 12:18:55 +0300

    Update UserController.java

--- src/main/java/web/controller/UserController.java
+++ src/main/java/web/controller/UserController.java
@@ -2,7 +2,9 @@ package web.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.*;
+import web.model.User;
 import web.service.UserService;
 
 @Controller
@@ -16,5 +18,45 @@ public class UserController {
         this.userService = userService;
     }
 
+    @GetMapping
+    public String listUsers(Model model) {
+        model.addAttribute("name", "Users:");
+        model.addAttribute("users", userService.listUsers());
+        return "users/index";
+    }
 
+    @GetMapping("/show")
+    public String showUsers(@RequestParam("id") int id, Model model) {
+        model.addAttribute("user", userService.show(id));
+        return "users/show";
+    }
+    @GetMapping("/new")
+    public String showCreateForm(Model model) {
+        model.addAttribute("user", new User());
+        return "users/new";
+    }
+
+    @PostMapping
+    public String createUser(@ModelAttribute("user") User user) {
+        userService.save(user);
+        return "redirect:/users";
+    }
+
+    @GetMapping("/edit")
+    public String showEditForm(@RequestParam("id") int id, Model model) {
+        model.addAttribute("user", userService.show(id));
+        return "users/edit";
+    }
+
+    @PostMapping("/edit")
+    public String updateUser(@ModelAttribute("user") User user) {
+        userService.update(user);
+        return "redirect:/users";
+    }
+
+    @PostMapping("/delete")
+    public String deleteUser(@RequestParam("id") int id) {
+        userService.delete(id);
+        return "redirect:/users";
+    }
 }
