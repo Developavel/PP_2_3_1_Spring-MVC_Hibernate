c44235820cddf780d5891a14f011b76c854ac7ed
Developavel <pa.mordovin@gmail.com>
2026-04-28 12:36:42 +0300

    Update User.java

--- src/main/java/web/model/User.java
+++ src/main/java/web/model/User.java
@@ -1,10 +1,20 @@
 package web.model;
 
+import javax.persistence.*;
 import java.util.Objects;
 
+@Entity
+@Table(name = "users")
 public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private int id;
+
+    @Column(name = "name")
     private String name;
+
+    @Column(name = "lastName")
     private String lastName;
 
     public User() {
