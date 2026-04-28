531afcc1211025ec6073df6e125a1849d5152e10
Developavel <pa.mordovin@gmail.com>
2026-04-28 16:38:23 +0300

    Update HelloController.java

--- src/main/java/web/controller/HelloController.java
+++ src/main/java/web/controller/HelloController.java
@@ -15,6 +15,9 @@ public class HelloController {
 		messages.add("Hello!");
 		messages.add("I'm Spring MVC + Hibernate application");
 		messages.add("CRUD application is ready!");
+		messages.add("Java version: " + System.getProperty("java.version"));
+		messages.add("Spring version: 5.3.14");
+		messages.add("Hibernate version: 5.6.15");
 		model.addAttribute("messages", messages);
 		return "index";
 	}
